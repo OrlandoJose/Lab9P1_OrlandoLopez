f266db0c5ea26636002de69bae84746675cc149f
OrlandoJose <gusritalempira08@gmail.com>
2026-06-19 15:29:29 -0600

    Segundo commit

--- src/lab9p1_orlandolopez/Lab9P1_OrlandoLopez.java
+++ src/lab9p1_orlandolopez/Lab9P1_OrlandoLopez.java
@@ -3,18 +3,129 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab9p1_orlandolopez;
-
+import java.util.Random;
+import javax.swing.JOptionPane; 
+import java.util.ArrayList;
 /**
  *
  * @author Home
  */
 public class Lab9P1_OrlandoLopez {
-
+    static  ArrayList<Soldado>soldado =new ArrayList<Soldado>();
+    static Random R=new Random();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opcion=1;
+        while(opcion!=3){
+            opcion=Integer.parseInt(JOptionPane.showInputDialog("1.-Creador de personajes\n2.-Dawn of the dead\n3.-Salir\nIngrese una opcion: "));
+            switch(opcion){
+                case 1:{
+                    int opcion2=1;
+                    while(opcion2!=5){
+                    opcion2=Integer.parseInt(JOptionPane.showInputDialog("1.-Crear Personaje\n2.-Modificar personaje\n3.-Eliminar Personaje\n4.-Listar Personajes\n5.-Salir\nIngrese una opcion: "));
+                    switch(opcion2){
+                        case 1:{
+                           Agregarpersonaje(soldado);                          
+                        }break;
+                        case 2:{
+                             if(soldado.isEmpty()==true){
+                                JOptionPane.showMessageDialog(null,"No ha creado personajes todavia");
+                                break;
+                             }else{
+                            Modificar(soldado);}
+                        }break;
+                        case 3:{
+                               if(soldado.isEmpty()==true){
+                                JOptionPane.showMessageDialog(null,"No ha creado personajes todavia");
+                                break;
+                               }else{
+                                   Eliminar(soldado);
+                               }
+                        }break;
+                        case 4:{
+                                if(soldado.isEmpty()==true){
+                                    JOptionPane.showMessageDialog(null,"No ha creado personajes todavia");
+                                break;
+                                }else{
+                            JOptionPane.showMessageDialog(null,Impresion(soldado));
+                                                        }
+                        }break;
+                        case 5:{
+                            
+                        }break;
+                        default:
+                            JOptionPane.showMessageDialog(null,"Ingrese una opcion valida");
+                    }
+                    }
+                }break;
+                case 2:{
+                    int t=Integer.parseInt(JOptionPane.showInputDialog("Ingrese las filas de la matriz"));
+                    char [][]m=LlenarMatriz(t);
+                    String Imprimir=mostrarMatriz(m);
+                    JOptionPane.showMessageDialog(null,"La matriz original es:\n"+Imprimir);
+                    
+                        
+                    
+                }break;
+                case 3:{                    
+                }break;
+                default:
+                JOptionPane.showMessageDialog(null,"Ingrese una opcion valida");
+            }
+        }         
     }
-    
+    public static void Agregarpersonaje(ArrayList<Soldado>s){
+        String nombre=JOptionPane.showInputDialog("Ingrese el nombre de su personaje: ");
+        String clase=JOptionPane.showInputDialog("Ingrese el nombre de su clase: ");
+        String habilidad=JOptionPane.showInputDialog("Ingrese el nombre de su habilidad: ");
+       Soldado temp=new Soldado(nombre,clase,habilidad);
+       soldado.add(temp);
+    }
+    public static String Impresion(ArrayList<Soldado>s){
+        String acum=" ";
+            for(int i=0;i<s.size();i++){
+           acum+=(i+1)+". "+s.get(i)+"\n";
+        }
+    return acum;}
+    public static void Modificar(ArrayList<Soldado>s){
+        String imprimir=Impresion(soldado);
+        int indice=Integer.parseInt(JOptionPane.showInputDialog(imprimir+"\nIngrese el indice del personaje que quiere cambiar: "));
+        indice--;
+        String nombre=JOptionPane.showInputDialog("Ingrese el nombre de su personaje: ");
+        String clase=JOptionPane.showInputDialog("Ingrese el nombre de su clase: ");
+        String habilidad=JOptionPane.showInputDialog("Ingrese el nombre de su habilidad: ");
+       Soldado temp=new Soldado(nombre,clase,habilidad);
+       soldado.set(indice,temp);    
+       JOptionPane.showMessageDialog(null,"El personaje modificado resulto en: \n"+temp);    
+    }
+    public static void Eliminar(ArrayList<Soldado>s){
+            String imprimir=Impresion(soldado);
+            int indice=Integer.parseInt(JOptionPane.showInputDialog(imprimir+"\nIngrese el indice del personaje que quiere eliminar: "));
+            indice--;
+            soldado.remove(indice);  
+            String imprimir2=Impresion(soldado);
+            JOptionPane.showMessageDialog(null,"El personaje se ha eliminado exitosamente\n"+imprimir2);
+    }
+
+    public static char[][] LlenarMatriz(int t){
+        String personajes="MHZ";
+        char[][]temp=new char[t][t];
+        for(int i=0;i<t;i++){
+            for(int j=0;j<t;j++){
+            temp[i][j]=personajes.charAt(R.nextInt(0,3));
+            }
+        }
+    return temp;}
+            public static String mostrarMatriz(char[][] mat) {
+            String acum=" ";      
+        for(int i=0;i<mat.length;i++) {
+            for(int j=0;j<mat[i].length;j++) {
+                acum+=mat[i][j]+"\t ";
+            }
+            acum+="\n";
+        }
+    return acum;}
 }
--- src/lab9p1_orlandolopez/Soldado.java
+++ src/lab9p1_orlandolopez/Soldado.java
@@ -4,10 +4,52 @@
  */
 package lab9p1_orlandolopez;
 
+import javax.swing.JOptionPane; 
 /**
  *
  * @author Home
  */
 public class Soldado {
-    
+    String nombre;
+    String clase;
+    String habilidad;
+    public Soldado(){
+}
+public Soldado(String nombre,String clase,String habilidad) {
+        this.nombre = nombre;
+        this.clase = clase;
+        this.habilidad = habilidad;       
+}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
+    public String Impresion() {
+        String mensaje=nombre + "-" + clase + "-" + habilidad;
+        return mensaje;
+    }
+    @Override
+    public String toString() {
+        return nombre + "-" + clase + "-" + habilidad;
+    }
+
 }
